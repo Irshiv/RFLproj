6411a6366b240e3baf37300b59d7c567fe9a27d7
Irshiv <79382183+Irshiv@users.noreply.github.com>
2022-02-28 12:23:37 -0800

    Navbar Change
    Added sticky navbar animation

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+// navbar sticky
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY >20){
+            $('.navbar').addClass("sticky")
+        }else{
+            $('.navbar').removeClass("sticky")
+        }
+    })
+})
 // typing animation script
 var typed = new Typed(".typing",{
     strings: ["Life", "Love", "You"],
