a5a7574fe72954080a25d4a8aef295d7f69ab1e6
Irshiv <79382183+Irshiv@users.noreply.github.com>
2022-03-01 17:24:24 -0800

    Cards and Recipe pages
    Changed cards on home page and added content to recipe page

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ var typed = new Typed(".typing",{
     backSpeed: 60,
     loop: true
 })
+function byID(id) {
+    return document.getElementById(id);
+  }
+  
+  byID("toggle").onclick = function() {
+    if (byID("container").classList.contains("closed")) {
+      byID("container").classList.remove("closed");
+    } else {
+      byID("container").classList.add("closed");
+    }
+  }
+  byID("toggle2").onclick = function() {
+    if (byID("container2").classList.contains("closed2")) {
+      byID("container2").classList.remove("closed2");
+    } else {
+      byID("container2").classList.add("closed2");
+    }
+  }
+  byID("toggle3").onclick = function() {
+    if (byID("container3").classList.contains("closed3")) {
+      byID("container3").classList.remove("closed3");
+    } else {
+      byID("container3").classList.add("closed3");
+    }
+  }
